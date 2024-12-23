afb05e36fcf4134511536346547f91612edf57a5
Chiichen <chiichen@qq.com>
2024-12-23 14:55:26 +0000

    feat: update home-manager to 24.11

--- chiichen-pc-wsl-24/flake.nix
+++ chiichen-pc-wsl-24/flake.nix
@@ -6,7 +6,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
